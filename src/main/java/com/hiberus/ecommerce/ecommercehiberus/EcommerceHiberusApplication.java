8c48ec6504542a96c0bbae247c4e923907ea0e3b
Camilo Valderrama <camilo.valderrama@areamovil.com.co>
2020-06-27 22:22:31 -0500

    Initial commit

---
+++ src/main/java/com/hiberus/ecommerce/ecommercehiberus/EcommerceHiberusApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus.ecommerce.ecommercehiberus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceHiberusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceHiberusApplication.class, args);
+	}
+
+}
